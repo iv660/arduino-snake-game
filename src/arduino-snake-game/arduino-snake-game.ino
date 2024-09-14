13c123458986abfdd3566e43990759f3898a5618
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2024-09-14 15:26:09 +0300

    feat: Experimental implementation of a snake eating an apple

---
+++ src/arduino-snake-game/arduino-snake-game.ino
@@ -0,0 +1,55 @@
+#include "TFT.h"
+#include "SPI.h"
+
+#define cs 10 // пин для подключения CS
+#define dc 8 // пин для подключения A0
+#define rst 9 // пин для подключения reset
+
+TFT screen = TFT(cs, dc, rst);
+
+void setup() {
+  screen.begin();
+
+  // цвет экрана ( r , g , b )
+  screen.background(0, 0, 0); 
+  // размер шрифта
+  screen.setTextSize(1);
+  // цвет текста ( r , g , b )
+  screen.stroke(255, 255, 255);
+
+  // вывод на экран текста с координатами x и y
+  screen.text("Snake/spider Game", 45, 20);
+  delay(1000);
+}
+
+void loop() {
+  // очистка экрана дисплея
+  screen.background(0, 0, 0);
+  // размер шрифта
+  screen.setTextSize(2);
+
+  // плавное изменение цвета текста
+  for (int i = 0; i <= 255; i=i+5) {
+    screen.stroke(i, 255 - i, 255);
+    screen.text("Snake/spider Game", 5, 50);
+    delay(5);
+  }
+  delay(1000);
+
+  // очистка экрана дисплея
+  screen.background(0, 0, 0);
+  // размер шрифта
+  screen.setTextSize(4);
+ 
+  // прокрутка текста справа - налево
+  for (int i = 130; i >= -360; i = i - 10) {
+    screen.background(0, 0, 0);
+    screen.stroke(0, 255, 0);
+    if (i >= 16) {
+      screen.text("@", 0, 40);
+    }
+    screen.text("########", i, 40);
+    delay(5);
+  }
+  delay(1000);
+}
